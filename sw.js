5b8149e71ffe7c07d7b8048b948c1b69023b39ec
Dongsoo Jung <100942621+DongsooJung@users.noreply.github.com>
2026-07-22 20:47:19 +0900

    Add customer FAQ and guide

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "stargate-edu-shop-v3";
-const ASSETS = ["/", "/index.html", "/checkout.html", "/catalog.js", "/payment-links.js", "/store.js", "/manifest.json",
+const CACHE = "stargate-edu-shop-v4";
+const ASSETS = ["/", "/index.html", "/checkout.html", "/catalog.js", "/payment-links.js", "/store.js", "/manifest.json", "/faq.html",
   "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
